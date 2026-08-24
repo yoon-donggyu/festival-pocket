260a0f640fd6b26536de771377563db9d700938c
yoon-donggyu <ydg6042@gmail.com>
2026-08-24 17:50:15 +0900

    Refresh Festival Pocket data cache version

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Festival Pocket loader: core app + additional festival dataset
 (function(){
   const core=document.createElement('script');
-  core.src='app-core.js?v=20260824-1';
+  core.src='app-core.js?v=20260824-2';
   core.onload=function(){
     const extra=document.createElement('script');
-    extra.src='festival-extra.js?v=20260824-1';
+    extra.src='festival-extra.js?v=20260824-2';
     document.body.appendChild(extra);
   };
   core.onerror=function(){console.error('Festival Pocket core load failed');};
